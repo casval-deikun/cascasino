ff5418a3cfad0fed8ee837af5f57a3579d111ac0
casval-deikun <64556091+casval-deikun@users.noreply.github.com>
2023-06-27 19:31:37 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Klaytn Transfer Service</title>
-<link rel="stylesheet" href="style.css">
-</head>
-<body>
-<h1>Klaytn Transfer Service</h1>
-<form action="index.js" method="post">
-<input type="text" name="address" placeholder="Recipient Address">
-<input type="number" name="amount" placeholder="Amount">
-<input type="submit" value="Send">
-</form>
-</body>
-</html>
+const Web3 = require('web3');
+const klaytnProvider = new Web3.providers.HttpProvider('https://api.klaytn.com');
+const web3 = new Web3(klaytnProvider);
+
+function getAddress() {
+  const ethereumAddress = window.ethereum.accounts[0];
+  return ethereumAddress;
+}
+
+function getBalance() {
+  const balance = web3.fromWei(web3.eth.getBalance(getAddress()), 'klay');
+  return balance;
+}
+
+function transfer(toAddress, amount) {
+  const transaction = web3.eth.sendTransaction({
+    from: getAddress(),
+    to: toAddress,
+    value: amount,
+  });
+
+  transaction.on('transactionHash', (hash) => {
+    console.log('Transaction hash:', hash);
+  });
+
+  transaction.on('receipt', (receipt) => {
+    console.log('Transaction receipt:', receipt);
+  });
+
+  transaction.send();
+}
+
+$(function() {
+  $('#transfer').on('click', function() {
+    const toAddress = $('#toAddress').val();
+    const amount = 100;
+
+    transfer(toAddress, amount);
+  });
+});
